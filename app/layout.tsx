69669de67d1f5d95967638718d1aad55b1a813ca
Unni Thottathil <unnikt@gmail.com>
2026-05-31 08:23:25 +1000

    refactor: replace Header with HeaderClient component for improved structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
+import HeaderClient from "./ui/HeaderClient";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Header />
+        <HeaderClient />
         {children}
       </body>
     </html>
---
+++ app/ui/HeaderClient.tsx
@@ -0,0 +1,6 @@
+"use client";
+import Header from "@/components/Header";
+
+export default function HeaderClient() {
+    return <Header />;
+}
